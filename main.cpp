2c49f8e7f2d24d99993655da6e684ea77ebe589f
9kazz <gkazunin@gmail.com>
2026-04-16 13:40:50 +0300

    12v: pragma parallel (double)

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 #define MAX(a,b) ((a) > (b) ? (a) : (b))
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
 
-#define SIMD_GROUP  8
+#define SIMD_GROUP  4
 
 enum EndStatus 
 {
@@ -23,8 +23,8 @@ int MandelCalculate (int width, int height, Color* Pixels_buf, Texture2D* Mandel
 
 int main()
 {
-    const int width  = 1200;
-    const int height = 1200;
+    const int width  = 1500;
+    const int height = 1500;
     
     InitWindow(width, height, "The Mandelbrot Set");
 
@@ -45,103 +45,108 @@ int main()
 
 int MandelCalculate(int width, int height, Color* Pixels_buf, Texture2D* MandelTexture) 
 {        
-    const float xMaxRef = 3;
-    const float yMaxRef = 3;
+    const double xMaxRef = 3;
+    const double yMaxRef = 3;
 
-    float xMax  = xMaxRef;
-    float yMax  = yMaxRef;
+    double xMax  = xMaxRef;
+    double yMax  = yMaxRef;
 
-    float x0ref = -xMax / 2;
-    float y0ref = -yMax / 2;
+    double x0ref = -xMax / 2;
+    double y0ref = -yMax / 2;
 
-    const float xZoom  = xMaxRef / 40;
-    const float yZoom  = yMaxRef / 40;
-    const float xShift = xMaxRef / 100;
-    const float yShift = yMaxRef / 100;
+    double xZoom  = 0;
+    double yZoom  = 0;
+    double xShift = 0;
+    double yShift = 0;
 
     while (!WindowShouldClose()) 
     {
         if(IsKeyDown(KEY_P))  { xMax -= xZoom;  yMax -= yZoom;  x0ref += xZoom / 2;  y0ref += yZoom / 2; }
         if(IsKeyDown(KEY_M))  { xMax += xZoom;  yMax += yZoom;  x0ref -= xZoom / 2;  y0ref -= yZoom / 2; }
 
+        xZoom  = xMax / 30;
+        yZoom  = yMax / 30;
+        xShift = xMax / 50;
+        yShift = yMax / 50;
+
         int screen_size = MIN(width, height);
-        const float dx  = xMax / screen_size;
-        const float dy  = yMax / screen_size;
+        const double dx  = xMax / screen_size;
+        const double dy  = yMax / screen_size;
             
         if(IsKeyDown(KEY_LEFT))   x0ref -= xShift;
         if(IsKeyDown(KEY_RIGHT))  x0ref += xShift;
         if(IsKeyDown(KEY_UP))     y0ref -= yShift;
         if(IsKeyDown(KEY_DOWN))   y0ref += yShift;
         
-        float x0 = x0ref;
-        float y0 = y0ref;
+        double x0 = x0ref;
+        double y0 = y0ref;
 
         int xPix = 0;
         int yPix = 0;
 
         double time_start = GetTime();
 
-        __m256 DotShift = _mm256_set_ps (7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0, 0.0);
-        __m256 DotInc   = _mm256_set1_ps(1.0);
-        __m256 ScaleX   = _mm256_set1_ps(dx);
-        __m256 R2max    = _mm256_set1_ps(4.0);
-        __m256 Nmax     = _mm256_set1_ps(255);
-        __m256 Zero     = _mm256_set1_ps(0.0);
+        __m256d DotShift = _mm256_set_pd (3.0, 2.0, 1.0, 0.0);
+        __m256d DotInc   = _mm256_set1_pd(1.0);
+        __m256d ScaleX   = _mm256_set1_pd(dx);
+        __m256d R2max    = _mm256_set1_pd(4.0);
+        __m256d Nmax     = _mm256_set1_pd(255);
+        __m256d Zero     = _mm256_set1_pd(0.0);
 
         #pragma omp parallel for
         for (yPix = 0;  yPix < height;  yPix++)
         {
             y0 = y0ref + yPix * dy;
-            __m256 Y0 = _mm256_set1_ps(y0);
+            __m256d Y0 = _mm256_set1_pd(y0);
             
             for (xPix = 0, x0 = x0ref;  xPix < width;  xPix += SIMD_GROUP, x0 += SIMD_GROUP * dx)
             {                   
-                __m256 X0     = _mm256_set1_ps(x0);
-                __m256 X0shft = _mm256_add_ps( X0, _mm256_mul_ps(DotShift, ScaleX) );
+                __m256d X0     = _mm256_set1_pd(x0);
+                __m256d X0shft = _mm256_add_pd( X0, _mm256_mul_pd(DotShift, ScaleX) );
 
-                __m256 X    = _mm256_set1_ps(0.0);
-                __m256 Y    = _mm256_set1_ps(0.0);
+                __m256d X    = _mm256_set1_pd(0.0);
+                __m256d Y    = _mm256_set1_pd(0.0);
   
-                __m256 N    = _mm256_set1_ps(0.0);
-                __m256 Nres = _mm256_set1_ps(0.0);
+                __m256d N    = _mm256_set1_pd(0.0);
+                __m256d Nres = _mm256_set1_pd(0.0);
 
-                __m256 R2res = _mm256_set1_ps(0.0);
+                __m256d R2res = _mm256_set1_pd(0.0);
 
                 for (int IsDotLeft = 0; ! IsDotLeft; )
                 {
-                    __m256 X2 = _mm256_mul_ps(X,  X);
-                    __m256 Y2 = _mm256_mul_ps(Y,  Y);
-                    __m256 XY = _mm256_mul_ps(X,  Y);
-                    __m256 R2 = _mm256_add_ps(X2, Y2);
+                    __m256d X2 = _mm256_mul_pd(X,  X);
+                    __m256d Y2 = _mm256_mul_pd(Y,  Y);
+                    __m256d XY = _mm256_mul_pd(X,  Y);
+                    __m256d R2 = _mm256_add_pd(X2, Y2);
 
-                    __m256 CmpMask1 = _mm256_cmp_ps (R2, R2max, _CMP_GT_OS);
-                    __m256 CmpMask2 = _mm256_cmp_ps (Nres, Zero, _CMP_EQ_OS);
-                           CmpMask1 = _mm256_and_ps (CmpMask1, CmpMask2);
+                    __m256d CmpMask1 = _mm256_cmp_pd (R2, R2max, _CMP_GT_OS);
+                    __m256d CmpMask2 = _mm256_cmp_pd (Nres, Zero, _CMP_EQ_OS);
+                           CmpMask1 = _mm256_and_pd (CmpMask1, CmpMask2);
 
-                    Nres  = _mm256_or_ps( Nres,  _mm256_and_ps(N, CmpMask1) );             
-                    R2res = _mm256_or_ps( R2res, _mm256_and_ps(R2, CmpMask1) );  
+                    Nres  = _mm256_or_pd( Nres,  _mm256_and_pd(N, CmpMask1) );             
+                    R2res = _mm256_or_pd( R2res, _mm256_and_pd(R2, CmpMask1) );  
 
-                    X = _mm256_add_ps( _mm256_sub_ps(X2, Y2), X0shft );
-                    Y = _mm256_add_ps( _mm256_add_ps(XY, XY), Y0 );
+                    X = _mm256_add_pd( _mm256_sub_pd(X2, Y2), X0shft );
+                    Y = _mm256_add_pd( _mm256_add_pd(XY, XY), Y0 );
 
-                    N = _mm256_add_ps(N, DotInc);
+                    N = _mm256_add_pd(N, DotInc);
 
-                    CmpMask1  = _mm256_cmp_ps (N, Nmax, _CMP_GE_OS);
-                    CmpMask2  = _mm256_cmp_ps (Nres, Zero, _CMP_EQ_OS);
-                    CmpMask1  = _mm256_and_ps (CmpMask1, CmpMask2);                    
-                    Nres      = _mm256_or_ps  (Nres, _mm256_and_ps(N, CmpMask1));
-                    R2res     = _mm256_or_ps( R2res, _mm256_and_ps(R2, CmpMask1) );  
+                    CmpMask1  = _mm256_cmp_pd (N, Nmax, _CMP_GE_OS);
+                    CmpMask2  = _mm256_cmp_pd (Nres, Zero, _CMP_EQ_OS);
+                    CmpMask1  = _mm256_and_pd (CmpMask1, CmpMask2);                    
+                    Nres      = _mm256_or_pd  (Nres, _mm256_and_pd(N, CmpMask1));
+                    R2res     = _mm256_or_pd( R2res, _mm256_and_pd(R2, CmpMask1) );  
  
-                    CmpMask2  = _mm256_cmp_ps (Nres, Zero, _CMP_EQ_OS);
-                    IsDotLeft = _mm256_testz_ps(CmpMask2, CmpMask2);
+                    CmpMask2  = _mm256_cmp_pd (Nres, Zero, _CMP_EQ_OS);
+                    IsDotLeft = _mm256_testz_pd(CmpMask2, CmpMask2);
                 }
 
                 for (int data_cnt = 0;  data_cnt < SIMD_GROUP;  data_cnt++) 
                 {
-                    int Rval = (int) R2res[data_cnt] * 15;
+                    int Rval = (int) R2res[data_cnt] * 20;
                     Pixels_buf[yPix * width + xPix + data_cnt] = {0, 0, Rval, 255};
 
-                    // int Nval = (int) Nres[data_cnt] * 4;
+                    // int Nval = (int) Nres[data_cnt];
                     // Pixels_buf[yPix * width + xPix + data_cnt] = {Nval, Nval, Nval, 255};
                 }
             }
